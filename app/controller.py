b487871d3e0362953884451c5a08ac67bde558aa
Michael Anderson <ratboyjim@gmail.com>
2020-10-30 21:41:46 +0000

    pvp html page added

--- app/controller.py
+++ app/controller.py
@@ -7,3 +7,12 @@ from app.models.game import *
 @app.route('/')
 def index():
     return render_template('index.html', title='RPS', players=players)
+
+@app.route('/rock/scissors')
+def player_v_player():
+    # Game.play_game(player_1, player_2)
+    return render_template('playervplayer.html', title='PvP', players=players)
+
+
+
+# methods=['POST']
