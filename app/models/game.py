a3531b674cf6a58a838c5a185d6eb581d79cd4f4
Michael Anderson <ratboyjim@gmail.com>
2020-10-30 20:08:18 +0000

    basic game working with tests

--- app/models/game.py
+++ app/models/game.py
@@ -1,17 +1,17 @@
 class Game():
     def play_game(player_1, player_2):
         if player_1.choice == "Rock" and player_2.choice == "Scissors":
-            return f"{player_1.name} is the winner because {player_1.choice} beats {player_2.choice}"
+            return f"{player_1.name} is the winner because {player_1.choice} smashes {player_2.choice}"
         elif player_1.choice == "Paper" and player_2.choice == "Rock":
-            return f"{player_1.name} is the winner because {player_1.choice} beats {player_2.choice}"
+            return f"{player_1.name} is the winner because {player_1.choice} covers {player_2.choice}"
         elif player_1.choice == "Scissors" and player_2.choice == "Paper":
-            return f"{player_1.name} is the winner because {player_1.choice} beats {player_2.choice}"
+            return f"{player_1.name} is the winner because {player_1.choice} cuts {player_2.choice}"
         elif player_1.choice == "Scissors" and player_2.choice == "Rock":
             return f"{player_2.name} is the winner because {player_2.choice} smashes {player_1.choice}"
         elif player_1.choice == "Rock" and player_2.choice == "Paper":
-            return f"{player_2.name} is the winner because {player_2.choice} beats {player_1.choice}"
+            return f"{player_2.name} is the winner because {player_2.choice} covers {player_1.choice}"
         elif player_1.choice == "Paper" and player_2.choice == "Scissors":
-            return f"{player_2.name} is the winner because {player_2.choice} beats {player_1.choice}"
+            return f"{player_2.name} is the winner because {player_2.choice} cuts {player_1.choice}"
         elif player_1.choice == player_2.choice:
             return None
 
--- app/tests/test_game.py
+++ app/tests/test_game.py
@@ -11,7 +11,7 @@ class TestGame(unittest.TestCase):
     def test_game_is_played__player1_wins_rock_beats_scissors(self):
         self.player1 = Player("Michael", "Rock")
         self.player2 = Player("Mizzle", "Scissors")
-        self.assertEqual("Michael is the winner because Rock beats Scissors", Game.play_game(self.player1, self.player2))
+        self.assertEqual("Michael is the winner because Rock smashes Scissors", Game.play_game(self.player1, self.player2))
 
     def test_game_is_working__player2_wins_rock_beats_scissors(self):
         self.player1 = Player("Michael", "Scissors")
@@ -21,7 +21,7 @@ class TestGame(unittest.TestCase):
     def test_game_is_working__player2_wins_scissors_beats_paper(self):
         self.player1 = Player("Michael", "Paper")
         self.player2 = Player("Mizzle", "Scissors")
-        self.assertEqual("Mizzle is the winner because Scissors beats Paper", Game.play_game(self.player1, self.player2))
+        self.assertEqual("Mizzle is the winner because Scissors cuts Paper", Game.play_game(self.player1, self.player2))
 
     def test_game_can_be_drawn(self):
         self.player1 = Player("Michael", "Paper")
