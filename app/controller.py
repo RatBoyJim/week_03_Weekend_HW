024ba3b2172cfc9e7a2e3560d644cbab57248041
Michael Anderson <ratboyjim@gmail.com>
2020-10-31 17:55:48 +0000

    kinda works

--- app/controller.py
+++ app/controller.py
@@ -10,11 +10,16 @@ def index():
 
 @app.route('/rock/scissors', methods=['GET', 'POST'])
 def player_v_player():
-    name1 = request.form['name1']
-    name2 = request.form['name2']
-    choice1 = request.form['choice1']
-    choice2 = request.form['choice2']
-    player1 = Player(name=name1, choice=choice1)
-    player2 = Player(name=name2, choice=choice2)
+    # name1 = request.form['name1']
+    # name2 = request.form['name2']
+    # choice1 = request.form['choice1']
+    # choice2 = request.form['choice2']
+    player1 = Player(name="Michael", choice="Rock")
+    player2 = Player(name="Marie", choice="Scissors")
     play_game = Game.play_game(player1, player2)
     return render_template('playervplayer.html', title='PvP', winner=play_game)
+
+@app.route('/results')
+def display_result():
+    return render_template('results.html', title='Results')
+
